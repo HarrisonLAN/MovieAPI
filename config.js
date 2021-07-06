f06e56e01d68cd7e9ee597f409d6147213458836
RobH59 <xlrobbielx59@gmail.com>
2021-07-06 23:30:52 +0100

    Case sensitive ENV

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 var config = {};
 
-config.DB_Connection = process.env.DB_Connection;
+config.DB_Connection = process.env.DB_CONNECTION;
 config.PORT = process.env.PORT;
 config.JWTTOKEN = process.env.TOKEN_SECRET;
 
