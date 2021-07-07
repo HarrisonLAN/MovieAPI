dd3463b878983491e355c54cd297d7b24e5abe63
RobH59 <xlrobbielx59@gmail.com>
2021-07-07 22:01:35 +0100

    fixing Node Modules

--- config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-var config = {};
-
-config.DB_Connection = process.env.DB_CONNECTION;
-config.PORT = process.env.PORT;
-config.JWTTOKEN = process.env.TOKEN_SECRET;
-
-module.exports = config;
